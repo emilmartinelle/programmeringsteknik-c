4d4fe006b120a8ff45e0dcaaa858c8ba583d7971
Emil Martinelle <emil.martinelle@studerande.plushhogskolan.se>
2020-08-28 15:20:11 +0200

    Chess app

---
+++ chess/Chess/ChessApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ChessApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            for (var y = 0; y < 8; y++)
+            {
+                for (var x = 0; x < 16; x++)
+
+                {
+                    int characterIndex = ((x / 2) + y) % 2;
+                    char character = characterIndex == 0 ? '░' : '▓';
+
+                    Console.Write(character);
+                }
+                    Console.Write('\n');
+                }
+                Console.ReadKey();
+
+            // string x = " 1 2 ";
+            
+            //foreach (char c in x)
+            //{
+            //    Console.Write(c);
+            //}
+            //for (int i = 0; i < x.Length; i++)
+            //{
+            //    Console.WriteLine(x[i]);
+
+
+
+
+
+                //Console.Write("▓");
+                //Console.Write("░");
+                //Console.ReadKey();
+            }
+        }
+    }
