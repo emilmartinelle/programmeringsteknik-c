8b3fb51e99c46eb3a491d278867de22261132abb
Emil Martinelle <emil.martinelle@studerande.plushhogskolan.se>
2020-10-08 09:18:37 +0200

    My changes

---
+++ Session05/Uppgift01Chess/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Uppgift01Chess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Använd Chess-koden som bas
+            // Rita ett schackbräde med hjälp av två tecken
+            // Man behöver använda % (modulo) 'restprodukten när man delar'
+        }
+    }
+}
---
+++ Uppgift01/Uppgift01Flagga/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Uppgift01Flagga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
